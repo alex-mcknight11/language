3813d6c05e81b824eb673ab571643539afe5a5fb
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-06-03 09:21:26 -0700

    add response test and js code

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,38 @@ $(document).ready(function() {
     function submitForm() {
       $('submitButton');
     }
+
+    const movie = $('input:radio[name=movie]:checked').val();
+
+		let cSum = 0;
+		let javascriptSum = 0;
+		let rubySum = 0;
+		let pythonSum = 0;
+		let wrenSum = 0;
+
+		if (movie === 'cOption') {
+			cSum = cSum + 1;
+		} else if (movie === 'javascriptOption') {
+			javascriptSum = javascriptSum + 1;
+		} else if (movie === 'rubyOption') {
+			rubySum = rubySum + 1;
+		} else if (movie === 'pythonOption') {
+			pythonSum = pythonSum + 1;
+		} else if (movie === 'wrenOption') {
+			wrenSum = wrenSum + 1;
+		}
+
+    if (
+			cSum >= javascriptSum &&
+			cSum >= rubySum &&
+			cSum >= pythonSum &&
+			cSum >= wrenSum
+		) {
+			$('#cResult').show();
+			$('javascriptResult').hide();
+			$('rubyResult').hide();
+			$('pythonResult').hide();
+			$('wrenResult').hide();
+
   });
 });
