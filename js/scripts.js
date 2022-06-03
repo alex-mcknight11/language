995a971aec8569a2698a553b4d4902480be20988
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-06-03 08:57:26 -0700

    set up page format

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#submitButton').submit(function  (event) {
+    event.preventDefault();
+
+    function submitForm() {
+      $('submitButton');
+    }
+  });
+});
